7789b74dd42031a52822dd9feec5a555284b1ac6
Zhuk Sergey <seregazhuk88@gmail.com>
2017-11-09 09:40:49 +0300

    use callbacks instead of mocks

--- src/TestCase.php
+++ src/TestCase.php
@@ -3,8 +3,7 @@
 namespace seregazhuk\React\PromiseTesting;
 
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
-use PHPUnit_Framework_MockObject_MockObject;
-use React\Promise\PromiseInterface;
+use React\Promise\Promise;
 use Clue\React\Block;
 use Exception;
 use React\EventLoop\LoopInterface;
@@ -12,102 +11,6 @@ use React\EventLoop\Factory as LoopFactory;
 
 class TestCase extends PHPUnitTestCase
 {
-    /**
-     * @param PromiseInterface $promise
-     * @param mixed $value
-     */
-    public function assertPromiseResolvesWith(PromiseInterface $promise, $value)
-    {
-        /** @var PromiseInterface $promise */
-        $promise->then(null, function($error) {
-            $this->assertNull($error);
-            $this->fail('promise rejected');
-        });
-
-        $promise->then($this->assertCallableCalledOnceWithArgs([$value]), $this->assertCallableNeverCalled());
-    }
-
-    /**
-     * @param PromiseInterface $promise
-     */
-    public function assertPromiseResolves(PromiseInterface $promise)
-    {
-        /** @var PromiseInterface $promise */
-        $promise->then(null, function($error) {
-            $this->assertNull($error);
-            $this->fail('Promise rejected');
-        });
-
-        $promise->then($this->assertCallableCalledOnce(), $this->assertCallableNeverCalled());
-    }
-
-    /**
-     * @param PromiseInterface $promise
-     * @param string $reasonExceptionClass
-     */
-    public function assertPromiseRejectsWith(PromiseInterface $promise, $reasonExceptionClass)
-    {
-        /** @var PromiseInterface $promise */
-        $promise->then(null, function($error) {
-            $this->assertNull($error);
-            $this->fail('Promise resolved');
-        });
-
-        $promise->then(
-            $this->assertCallableNeverCalled(),
-            $this->assertCallableCalledOnceWithObjectOf($reasonExceptionClass)
-        );
-    }
-
-    /**
-     * @return PHPUnit_Framework_MockObject_MockObject|callable
-     */
-    public function assertCallableCalledOnce()
-    {
-        $mock = $this->getMockBuilder(CallableStub::class)->getMock();
-        $mock->expects($this->once())->method('__invoke');
-
-        return $mock;
-    }
-
-    /**
-     * @param array $arguments
-     * @return PHPUnit_Framework_MockObject_MockObject|callable
-     */
-    public function assertCallableCalledOnceWithArgs(array $arguments = [])
-    {
-        $mock = $this->getMockBuilder(CallableStub::class)->getMock();
-        $mock->expects($this->once())->method('__invoke')->with(...$arguments);
-
-        return $mock;
-    }
-
-    /**
-     * @param string $class
-     * @return PHPUnit_Framework_MockObject_MockObject|callable
-     */
-    public function assertCallableCalledOnceWithObjectOf($class)
-    {
-        $mock = $this->getMockBuilder(CallableStub::class)->getMock();
-
-        $mock->expects($this->once())
-            ->method('__invoke')
-            ->with($this->isInstanceOf($class));
-
-        return $mock;
-    }
-
-    /**
-     * @return PHPUnit_Framework_MockObject_MockObject|callable
-     */
-    public function assertCallableNeverCalled()
-    {
-        $mock = $this->getMockBuilder(CallableStub::class)->getMock();
-        $mock->expects($this->never())->method('__invoke');
-
-        return $mock;
-    }
-
     /**
      * @var LoopInterface
      */
@@ -119,20 +22,68 @@ class TestCase extends PHPUnitTestCase
     }
 
     /**
-     * @param PromiseInterface $promise
+     * @param Promise $promise
+     * @param mixed $value
+     */
+    public function assertPromiseResolvesWith(Promise $promise, $value)
+    {
+        $promise->done(
+            function($resolvedValue) use ($value) {
+                $this->assertEquals($resolvedValue, $value, 'Failed asserting that promise resolves with a specified value.');
+            },
+            function() {
+                $this->fail('Failed asserting that promise resolves with a specified value. Promise was rejected.');
+            });
+    }
+
+    /**
+     * @param Promise $promise
+     */
+    public function assertPromiseResolves(Promise $promise)
+    {
+        $promise->done(null, function(){
+            $this->fail('Failed asserting that promise resolves. Promise was rejected.');
+        });
+    }
+
+    public function assertPromiseRejects(Promise $promise)
+    {
+        $promise->done(function() {
+            $this->fail('Failed asserting that promise rejects. Promise was resolved.');
+        });
+    }
+
+    /**
+     * @param Promise $promise
+     * @param string $reasonExceptionClass
+     */
+    public function assertPromiseRejectsWith(Promise $promise, $reasonExceptionClass)
+    {
+        $promise->done(function() {
+            $this->fail('Failed asserting that promise rejects. Promise was resolved.');
+        }, function($reason) use ($reasonExceptionClass) {
+            $this->assertInstanceOf(
+                $reasonExceptionClass,
+                $reason,
+                'Failed asserting that promise rejects with a specified reason.');
+        });
+    }
+
+    /**
+     * @param Promise $promise
      * @return mixed
      * @throws Exception
      */
-    public function waitForPromiseResolves(PromiseInterface $promise)
+    public function waitForPromiseResolves(Promise $promise)
     {
         return Block\await($promise, $this->loop);
     }
 
     /**
-     * @param PromiseInterface $promise
+     * @param Promise $promise
      * @return Exception
      */
-    public function waitForPromiseRejects(PromiseInterface $promise)
+    public function waitForPromiseRejects(Promise $promise)
     {
         try {
             Block\await($promise, $this->loop);
@@ -145,26 +96,26 @@ class TestCase extends PHPUnitTestCase
 
     /**
      * @param mixed $value
-     * @param PromiseInterface $promise
+     * @param Promise $promise
      * @throws Exception
      */
-    public function waitForPromiseResolvesWith(PromiseInterface $promise, $value)
+    public function waitForPromiseResolvesWith(Promise $promise, $value)
     {
         $this->assertEquals($value, $this->waitForPromiseResolves($promise));
     }
 
     /**
-     * @param PromiseInterface $promise
+     * @param Promise $promise
      * @param string $reasonExceptionClass
      */
-    public function waitPromiseRejectsWith(PromiseInterface $promise, $reasonExceptionClass)
+    public function waitPromiseRejectsWith(Promise $promise, $reasonExceptionClass)
     {
         $reason = $this->waitForPromiseRejects($promise);
 
         $this->assertInstanceOf(
             $reasonExceptionClass,
             $reason,
-            "Promise should reject with instance of $reasonExceptionClass, got " . get_class($reason)
+            'Failed asserting that promise rejects with a specified reason.'
         );
     }
 }
