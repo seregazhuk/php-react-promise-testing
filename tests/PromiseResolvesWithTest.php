3cbbbc3d2f8da9458f5dfb75ecc973755f51313d
Zhuk Sergey <seregazhuk88@gmail.com>
2018-01-05 18:13:26 +0300

    add promise resolves with rests

--- tests/PromiseResolvesWithTest.php
+++ tests/PromiseResolvesWithTest.php
@@ -3,7 +3,7 @@
 namespace seregazhuk\React\PromiseTesting\tests;
 
 use React\Promise\Deferred;
-use React\Promise\Promise;
+use function React\Promise\Timer\resolve;
 use seregazhuk\React\PromiseTesting\TestCase;
 
 class PromiseResolvesWithTest extends TestCase
@@ -21,8 +21,57 @@ class PromiseResolvesWithTest extends TestCase
                 '/Failed asserting that promise resolves with a specified value/',
                 $exception->getMessage()
             );
+
             $this->assertRegExp(
                 '/Failed asserting that .+ matches expected .+/',
+                $exception->getMessage()
+            );
+        }
+    }
+
+    /** @test */
+    public function it_fails_when_promise_rejects()
+    {
+        try {
+            $deferred = new Deferred();
+
+            $deferred->reject();
+            $this->assertPromiseResolvesWith($deferred->promise(), 1);
+        } catch (\PHPUnit_Framework_Exception $exception) {
+            $this->assertRegExp(
+                '/Failed asserting that promise resolves with a specified value/',
+                $exception->getMessage()
+            );
+
+            $this->assertRegExp(
+                '/Promise was rejected/',
+                $exception->getMessage()
+            );
+        }
+    }
+
+    /** @test */
+    public function it_fails_when_promise_doesnt_resolve_in_a_specified_timeout()
+    {
+        try {
+            $deferred = new Deferred();
+
+            $deferred->reject();
+            $promise = resolve(3, $this->loop);
+
+            $promise->then(function() use ($deferred){
+                $deferred->resolve();
+            });
+
+            $this->assertPromiseResolvesWith($promise, 1);
+        } catch (\PHPUnit_Framework_Exception $exception) {
+            $this->assertRegExp(
+                '/Failed asserting that promise resolves with a specified value/',
+                $exception->getMessage()
+            );
+
+            $this->assertRegExp(
+                '/Promise was rejected by timeout/',
                 $exception->getMessage()
             );
         }
