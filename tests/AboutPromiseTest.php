a5a2f7d532237f2e2f2068cb6217ca8199e111dd
ace411 <lochbm@live.com>
2020-06-20 02:21:34 +0300

    Applied style fixes

--- tests/AboutPromiseTest.php
+++ tests/AboutPromiseTest.php
@@ -8,61 +8,61 @@ use seregazhuk\React\PromiseTesting\TestCase;
 
 final class AboutPromiseTest extends TestCase
 {
-	/** @test */
-	public function predicate_function_reveals_what_is_true_about_promise(): void
-	{
-		try {
-			$fst = new Deferred;
-      		$fst->resolve(23);
+    /** @test */
+    public function predicate_function_reveals_what_is_true_about_promise(): void
+    {
+        try {
+            $fst = new Deferred;
+            $fst->resolve(23);
 
-			$snd = new Deferred;
-			$snd->reject(new Exception('An error'));
+            $snd = new Deferred;
+            $snd->reject(new Exception('An error'));
 
-			$thd = new Deferred;
-			$thd->reject('Another error');
+            $thd = new Deferred;
+            $thd->reject('Another error');
 
-			$this->assertTrueAboutPromise($fst->promise(), function ($val): bool {
-				return is_int($val) && $val > 20;
-			});
-			$this->assertTrueAboutPromise($fst->promise(), 'is_string');
+            $this->assertTrueAboutPromise($fst->promise(), function ($val): bool {
+                return is_int($val) && $val > 20;
+            });
+            $this->assertTrueAboutPromise($fst->promise(), 'is_string');
 
-			$this->assertTrueAboutPromise($snd->promise(), 'is_string');
+            $this->assertTrueAboutPromise($snd->promise(), 'is_string');
 
-			$this->assertTrueAboutPromise($thd->promise(), 'is_string');
-		} catch (Exception $exception) {
-			$this->assertRegExp(
-				'/Failed asserting that .+/',
-				$exception->getMessage()
-			);
-		}
-	}
+            $this->assertTrueAboutPromise($thd->promise(), 'is_string');
+        } catch (Exception $exception) {
+            $this->assertRegExp(
+                '/Failed asserting that .+/',
+                $exception->getMessage()
+            );
+        }
+    }
 
-	/** @test */
-	public function predicate_function_reveals_what_is_false_about_promise(): void
-	{
-		try {
-			$fst = new Deferred;
-			$fst->resolve(23);
+    /** @test */
+    public function predicate_function_reveals_what_is_false_about_promise(): void
+    {
+        try {
+            $fst = new Deferred;
+            $fst->resolve(23);
 
-			$snd = new Deferred;
-			$snd->reject(new Exception('An error'));
+            $snd = new Deferred;
+            $snd->reject(new Exception('An error'));
 
-			$thd = new Deferred;
-			$thd->reject('Another error');
+            $thd = new Deferred;
+            $thd->reject('Another error');
 
-			$this->assertFalseAboutPromise($fst->promise(), function ($val): bool {
-				return is_int($val) && $val > 20;
-			});
-			$this->assertFalseAboutPromise($fst->promise(), 'is_string');
+            $this->assertFalseAboutPromise($fst->promise(), function ($val): bool {
+                return is_int($val) && $val > 20;
+            });
+            $this->assertFalseAboutPromise($fst->promise(), 'is_string');
 
-			$this->assertFalseAboutPromise($snd->promise(), 'is_string');
+            $this->assertFalseAboutPromise($snd->promise(), 'is_string');
 
-			$this->assertFalseAboutPromise($thd->promise(), 'is_int');
-		} catch (Exception $exception) {
-			$this->assertRegExp(
-				'/Failed asserting that .+/',
-				$exception->getMessage()
-			);
-		}
-	}
+            $this->assertFalseAboutPromise($thd->promise(), 'is_int');
+        } catch (Exception $exception) {
+            $this->assertRegExp(
+                '/Failed asserting that .+/',
+                $exception->getMessage()
+            );
+        }
+    }
 }
